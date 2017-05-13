0a19a474db96d6e44e116e4a5ed031f1f829895e
Edward Mendoza <lalo.garibo90@gmail.com>
2017-05-13 05:34:07 +0000

    Created new template and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- # devise_for :views
   devise_for :users
+  resources :tasks 
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
