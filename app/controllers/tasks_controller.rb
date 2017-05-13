6e32cf4d04086f2a4e3cf6d2d6c4a95f19c7cad9
Edward Mendoza <lalo.garibo90@gmail.com>
2017-05-13 22:26:52 +0000

    Created user-task association and updated task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,51 @@
+
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_task, only:[:edit, :update, :show, :destroy]
+    def index
+    @task = current_user.tasks
+  end
   
-def index
-  @task = Task.all
-end
-  
-def new
+    def new
     @task = Task.new 
   end
-  
-  def edit
-  
-end
+    def edit
+      end
 
-def create
-    @task = Task.new(task_params)
-  if  @task.save
-    flash[:notice] = "Task was successfully created"
-    redirect_to task_path(@task)
-  else 
-    render 'new'
+  def create
+    @task = current_user.tasks.new(task_params)
+    if  @task.save
+      flash[:notice] = "Task was successfully created"
+      redirect_to task_path(@task)
+    else 
+      render 'new'
+    end
   end
-end
 
-def update
- 
-  if @task.update(task_params)
-    flash[:notice] = "Task was successfully updated"
-    redirect_to task_path(@task)
-  else
-    render 'edit'
+  def update
+        if @task.update(task_params)
+      flash[:notice] = "Task was successfully updated"
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
   end
-end
 
-def show
-  
-end
+  def show
+      end
 
-def destroy
-  if @task.destroy
-    flash[:notice] = "Task #{@task.content}, was successfully destroyed"
-    redirect_to tasks_path
+  def destroy
+    if @task.destroy
+      flash[:notice] = "Task #{@task.content}, was successfully destroyed"
+      redirect_to tasks_path
+    end
   end
-end
 
-  
-  
-  private
-  
-  def task_params
+      private
+    def task_params
     params.require(:task).permit(:content)
   end
-  
-  def find_task
+    def find_task
     @task = Task.find(params[:id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: {minimum: 5, maximum: 255}
+  belongs_to :user
 end
