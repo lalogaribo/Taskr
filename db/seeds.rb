1af1e91cb002495994931d454dc186e1d21dfe57
Edward Mendoza <lalo.garibo90@gmail.com>
2017-05-13 04:45:12 +0000

    Added task table and model and some tasks using seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |tasks|
+  Task.create!(
+    content: "Generate new tasks via seeders #{tasks}"
+  )
+end
+puts "5 tasks were created"
