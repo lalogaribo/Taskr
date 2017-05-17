a2a87c73e3a3369f864d2845488decca1af237fb
Edward Mendoza <lalo.garibo90@gmail.com>
2017-05-17 04:52:55 +0000

    Added custom actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,17 @@
 
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_task, only:[:edit, :update, :show, :destroy]
-  
-    def index
+  before_action :find_task, only:[:edit, :update, :show, :destroy, :change]
+  def index
     @to_do = current_user.tasks.where(state: 'to_do')
     @doing = current_user.tasks.where(state: 'doing')
     @done = current_user.tasks.where(state: 'done')
   end
-  
     def new
     @task = Task.new 
   end
-    def edit
-      end
+  def edit
+  end
 
   def create
     @task = current_user.tasks.new(task_params)
@@ -26,7 +24,7 @@ class TasksController < ApplicationController
   end
 
   def update
-        if @task.update(task_params)
+    if @task.update(task_params)
       flash[:notice] = "Task was successfully updated"
       redirect_to task_path(@task)
     else
@@ -35,7 +33,7 @@ class TasksController < ApplicationController
   end
 
   def show
-      end
+  end
 
   def destroy
     if @task.destroy
@@ -43,12 +41,18 @@ class TasksController < ApplicationController
       redirect_to tasks_path
     end
   end
+  
+  def change
+    @task.update_attributes(state: params[:state])
+    flash[:notice] = "Task status was successfully changed"
+    redirect_to task_path
+  end
 
-      private
-    def task_params
+  private
+  def task_params
     params.require(:task).permit(:content, :state)
   end
-    def find_task
+  def find_task
     @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks 
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
